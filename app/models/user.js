1fa1499fd289e7c914946d5b759c30c46187f9d7
Dorje <rtorchiiw@gmail.com>
2019-03-05 16:55:34 +0545

    add password field

--- app/models/user.js
+++ app/models/user.js
@@ -1,12 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
   }, {});
   User.associate = function(models) {
     // associations can be defined here
   };
+
   return User;
 };
