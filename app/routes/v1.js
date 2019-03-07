dcc07928c9e9c08e02fc54674c65af9966c466db
Dorje <rtorchiiw@gmail.com>
2019-03-07 17:38:43 +0545

    add route to resister user

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -1,6 +1,7 @@
 import * as UserController from '../controllers/user_controller';
 module.exports = (app) => {
   app.get('/v1/users', UserController.getAll )
+  app.post('/v1/users/register', UserController.register)
   // app.get('/v1/users/1', function( req, res ){
   //   res.send('ffff')
   // })
